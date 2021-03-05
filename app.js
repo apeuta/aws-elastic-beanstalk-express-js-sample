62aa897064cc4a5c8ceb6fd9b5d224c44f69ecb1
apeuta <57470231+apeuta@users.noreply.github.com>
2021-03-05 15:30:04 +0700

    update greetings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CI/CD Trial is running dude!'));
+app.get('/', (req, res) => res.send('Thank God it is Friday!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
