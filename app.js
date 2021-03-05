c8f462fc9fad058096a52d3e7d71726eb224687e
unknown <andra@vibicloud.com>
2021-03-05 14:22:50 +0700

    CI CD Trial

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Thank God It is Friday!! How Are Youu??'));
+app.get('/', (req, res) => res.send('CI/CD Trial is running dude!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
